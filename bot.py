e67f09b78da15acccd79cea7b71d09da59190c4c
dimoha_zadira <dimikson4@gmail.com>
2022-06-19 19:02:50 +0300

    завелась бандура завелась!

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import asyncio
+from hashlib import new
 import os
 import telebot
 import logging
@@ -16,28 +17,39 @@ bot = AsyncTeleBot(token)
 users_playlists = defaultdict(set)
 playlists_tracks = {}
 
-client = Client().init()
+
+client = Client()
 
 print("ready!")
 def extract_arg(arg):
     return arg.split()[1:]
 
-def check_playlist_update(playlist):
-    playlist_id = playlist.text.split('/')[-1]
-    user = playlist.text.split('/')[-3]
-    return
+# Возвращает разность количества треков на сервере и последнего запомненного состояния, обновляет последнее запомненное состояние
+def check_playlist_update(playlist_name):
+    playlist_id = playlist_name.split('/')[-1]
+    user = playlist_name.split('/')[-3]
+    logging.info(f"playlist_id {playlist_id} user {user}")
+    playlist = client.users_playlists(playlist_id, user)
+
+    old_track_count = playlists_tracks[playlist_name]
+    playlists_tracks[playlist_name] = playlist.track_count
+    return playlist.track_count - old_track_count
 
 @bot.message_handler(commands=['add_playlist'])
 async def add_playlist(message):
+    playlist_name = "/".join(extract_arg(message.text))
+
     playlist_id = message.text.split('/')[-1]
     user = message.text.split('/')[-3]
-    playlist = str(extract_arg(message.text))
-    logging.info(f"adding {message.chat.id}: {playlist}")
-    
-    users_playlists[message.chat.id].add(playlist)
 
-    playlists_tracks[playlist] = client.users_playlists(playlist_id, user).track_count
-    logging.info(f"added playlists_tracks: {playlist} : {playlists_tracks[playlist]}")
+    logging.info(f"adding {message.chat.id}: {playlist_name}")
+    
+    users_playlists[message.chat.id].add(playlist_name)
+
+    playlist = client.users_playlists(playlist_id, user)
+    playlists_tracks[playlist_name] = playlist.track_count
+
+    logging.info(f"added playlists_tracks: {playlist_name} : {playlists_tracks[playlist_name]}")
 
     await bot.reply_to(message, "success!")
 
@@ -55,10 +67,23 @@ I am here to echo your kind words back to you. Just say anything nice and I'll s
 """)
 
 
-@bot.message_handler()
-async def echo_message(message):
-    await bot.reply_to(message, message.text)
 
-asyncio.run(bot.polling())
+async def polling():
+    while True:
+        for user, playlists in users_playlists.items():
+            logging.info(f"checking user {user}, playlists {playlists}")
+            for playlist in playlists:
+                if check_playlist_update(playlist) != 0:
+                    logging.info(f"sending update msg to user {user}, playlist {playlist}")
+                    await bot.send_message(user, f"there is an update in playlist {playlist}!")
+        await asyncio.sleep(5)
+
+
+async def main():
+    client.init() 
+    await asyncio.gather(bot.infinity_polling(), polling())
+
+
+asyncio.run(main())
 
 
