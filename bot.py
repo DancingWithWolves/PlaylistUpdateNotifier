319160376d84b12465ccb9176b13dae48def60be
dimoha_zadira <dimikson4@gmail.com>
2022-07-10 17:02:14 +0300

    убрал повторуную инициализацию

--- bot.py
+++ bot.py
@@ -252,14 +252,7 @@ async def main():
 # Запуск основого лупа
 while 1:
     try:
-        logging.info("starting an app!")
-        
-        token = os.getenv('TELEGRAM_BOT_TOKEN')
-        bot = AsyncTeleBot(token)
-
-        ym_token = os.getenv('YANDEX_MUSIC_TOKEN')
-        client = ClientAsync(ym_token)
-        
+        logging.info("starting an app!")        
         asyncio.run(main())
     except Exception as error:
         logging.error(error)
